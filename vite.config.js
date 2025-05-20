cbc11019fb3fb1daade8946ea812596b89ccac91
BrunoCaian <brunocaian81@gmail.com>
2025-05-19 23:25:01 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('firebase')) return 'firebase'
-          if (id.includes('react-toastify')) return 'toastify'
-          if (id.includes('lottie-react')) return 'lottie'
-          if (id.includes('react-loader-spinner')) return 'loader'
-          if (id.includes('react')) return 'react'
-        }
-      }
-    },
-  },
-  preview: {
-    historyApiFallback: true,
   }
 })
